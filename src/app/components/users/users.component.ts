1713d0043627175bc78b517e2d50cf6626f6d1d2
Phakama Ntshewula <ntshewulap@gmail.com>
2018-10-01 12:57:54 +0200

    Template Driven Forms - Submission

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,63 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../../models/User';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { User } from "../../models/User";
 
 @Component({
-	selector: 'app-users',
-	templateUrl: './users.component.html',
-	styleUrls: ['./users.component.css']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.css"]
 })
 export class UsersComponent implements OnInit {
-	user: User = {
-		firstName: '',
-		lastName: '',
-		email: ''
-	};
-	users: User[];
-	showExtended = true;
-	loaded = true;
-	enableAdd = false;
-	showUserForm: false;
+  user: User = {
+    firstName: "",
+    lastName: "",
+    email: ""
+  };
+  users: User[];
+  showExtended = true;
+  loaded = true;
+  enableAdd = false;
+  showUserForm: false;
+  @ViewChild("userForm")
+  form: any;
 
-	constructor() { }
+  constructor() {}
 
-	ngOnInit() {
-		this.users = [
-			{
-				firstName: 'John',
-				lastName: 'Doe',
-				email: 'john@gmail.com',
-				isActive: true,
-				registered: new Date('01/02/2018 08:30:00'),
-				hide: true
-			},
-			{
-				firstName: 'Lee',
-				lastName: 'Smith',
-				email: 'lee@yahoo.com',
-				isActive: false,
-				registered: new Date('05/12/2018 09:30:00'),
-				hide: true
-			},
-			{
-				firstName: 'Beke',
-				lastName: 'Pengas',
-				email: 'beke@hotmail.com',
-				isActive: true,
-				registered: new Date('15/02/2018 09:00:00'),
-				hide: true
-			}
-		];
-		this.loaded = true;
+  ngOnInit() {
+    this.users = [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        email: "john@gmail.com",
+        isActive: true,
+        registered: new Date("2018/09/03 08:30:00"),
+        hide: true
+      },
+      {
+        firstName: "Lee",
+        lastName: "Smith",
+        email: "lee@yahoo.com",
+        isActive: false,
+        registered: new Date("2018/12/04 09:30:00"),
+        hide: true
+      },
+      {
+        firstName: "Beke",
+        lastName: "Pengas",
+        email: "beke@hotmail.com",
+        isActive: true,
+        registered: new Date("2018/03/01 09:00:00"),
+        hide: true
+      }
+    ];
+    this.loaded = true;
+  }
 
-	}
+  // addUser() {
+  // 	this.users.unshift(this.user);
+  // 	this.user.isActive = true;
+  // 	this.user.registered = new Date();
+  // }
 
-	// addUser() {
-	// 	this.users.unshift(this.user);
-	// 	this.user.isActive = true;
-	// 	this.user.registered = new Date();
-	// }
+  onSubmit({ value, valid }: { value: User; valid: boolean }) {
+    if (!valid) {
+      console.log("Invalid User");
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
 
-	onSubmit(event) {
-		event.preventDefault();
-	}
+      this.users.unshift(value);
+
+      this.form.reset();
+    }
+  }
 }
